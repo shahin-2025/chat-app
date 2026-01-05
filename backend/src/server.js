736e353f878dbae28419401fba7b8cce7c885a5c
KobirDev <shahinkobirmail@gmail.com>
2026-01-06 00:04:52 +0600

    ready for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,24 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+
+if(process.env.node_env === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
+    });
+}
 
 app.use(cors());
 app.use(express.json());
